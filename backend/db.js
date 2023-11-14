0d5df62fc807a6681e8ff8c53f175788e276ace0
abhi5235 <palabhi609@gmail.com>
2023-11-14 15:41:00 +0530

    push

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
 
-const mongoURI = 
+const mongoURI = "mongodb://127.0.0.1:27017/mydatabse";
 
-const connectToMongo = () => {
-  mongoose.connect(mongoURI, ()=>{
-   
-console.log("Connected to Mongo Successfully");
-  })
- 
-}
+const connectToMongo = async () => {
+  try {
+    await mongoose.connect(mongoURI);
+    console.log('Connected to MongoDB');
+    // Your code here
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
+};
 
 module.exports = connectToMongo;
